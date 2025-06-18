195433d162e31540f83c57103415fb53a19bd7db
李忠軒 <aken1023@gmail.com>
2025-06-19 00:13:22 +0800

    Convert remaining UI text to Traditional Chinese

--- packages/components/src/chatbox/thought-chain/index.tsx
+++ packages/components/src/chatbox/thought-chain/index.tsx
@@ -110,7 +110,7 @@ export default function ThoughtChain(props: IThoughtChainProps) {
 														className="cursor-pointer"
 														onClick={async () => {
 															await copyToClipboard(item.tool_input)
-															message.success('复制成功')
+                                                                               message.success('複製成功')
 														}}
 													/>
 												</div>
@@ -126,7 +126,7 @@ export default function ThoughtChain(props: IThoughtChainProps) {
 														className="cursor-pointer"
 														onClick={async () => {
 															await copyToClipboard(item.observation)
-															message.success('复制成功')
+                                                                               message.success('複製成功')
 														}}
 													/>
 												</div>
